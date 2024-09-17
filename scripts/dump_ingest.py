7ce3dae61224056a58d0ca48e7d684b05011a4cd
Jean-Pierre Matsumoto <jpmat296@gmail.com>
2024-09-18 01:04:47 +0200

    Add dump scripts

---
+++ scripts/dump_ingest.py
@@ -0,0 +1,18 @@
+from elasticsearch import Elasticsearch
+
+import json
+
+es = Elasticsearch('https://elastic:change_me@localhost:9200/', verify_certs=False)
+
+igs = es.ingest.get_pipeline(id='logs-panw.*')
+print(f"{len(igs.body.keys())} ingest pipelines found.")
+
+with open("dev_tools_ingest.txt", "w") as outfile: 
+    outfile.write("# After below commands, 4 pipelines exist")
+    outfile.write("# To check:\n")
+    outfile.write("GET _ingest/pipeline/*-infoblox_nios.*?filter_path=*.description\n")
+    for ig in igs.body.keys():
+        outfile.write("\n")
+        outfile.write(f"PUT _index_template/{ig}\n")
+        json.dump(igs[ig], outfile, indent=2)
+        outfile.write("\n")
---
+++ scripts/dump_kibana_objects.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+base_url = 'https://elastic:change_me@localhost:5601/'
+
+res = requests.post(url=f"{base_url}api/saved_objects/_export", verify=False, json={
+        'type': '*'
+    }, headers={
+        'Content-Type': 'application/json; Elastic-Api-Version=2023-10-31',
+        'kbn-xsrf': 'string',
+    }
+)
+
+tag = 0
+search = 0
+dashboards = 0
+# Last line is summary. Ignore it.
+with open("objects.ndjson", "w") as outfile:
+    for line in res.text.splitlines()[:-1]:
+        obj = json.loads(line)
+        if 'panw' in obj['id']:
+            outfile.write(line)
+            if obj['type'] == 'tag':
+                tag += 1
+            elif obj['type'] == 'dashboard':
+                dashboards += 1
+            elif obj['type'] == 'search':
+                search += 1
+            else:
+                print(f"Unknown object type: {obj['type']}")
+
+print("Saved objects exported in file objects.ndjson")
+print(f"{tag} tags exported")
+print(f"{search} searches exported")
+print(f"{dashboards} dashboards exported")
