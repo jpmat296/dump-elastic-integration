2648b7cd48ab457823c93e1c721684de65f72daf
Jean-Pierre Matsumoto <jpmat296@gmail.com>
2024-10-17 14:53:54 +0200

    Integration cisco_ise version 1.23.2

--- scripts/dump_index_templates.py
+++ scripts/dump_index_templates.py
@@ -4,17 +4,17 @@ import json
 
 es = Elasticsearch('https://elastic:change_me@localhost:9200/', verify_certs=False)
 
-cts = es.cluster.get_component_template(name='logs-panw.panos@package')
+cts = es.cluster.get_component_template(name='logs-cisco_ise.log@package')
 print(f"{len(cts['component_templates'])} component templates found.")
 
-tmpls = es.indices.get_index_template(name='logs-panw.panos')
+tmpls = es.indices.get_index_template(name='logs-cisco_ise.log')
 print(f"{len(tmpls['index_templates'])} index templates found.")
 
 with open("dev_tools_index.txt", "w") as outfile: 
     outfile.write("# After below commands, 1 component template  + 1 index template exist\n")
     outfile.write("# To check:\n")
-    outfile.write("GET _component_template/*-panw.*?filter_path=*.name\n")
-    outfile.write("GET _index_template/*-panw.*?filter_path=*.name\n")
+    outfile.write("GET _component_template/*-cisco_ise.*?filter_path=*.name\n")
+    outfile.write("GET _index_template/*-cisco_ise.*?filter_path=*.name\n")
     for ct in cts['component_templates']:
         outfile.write("\n")
         outfile.write(f"PUT _component_template/{ct['name']}\n")
