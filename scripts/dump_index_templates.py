d016b37ef37a415956be1a5b64f02cddac919e50
Jean-Pierre Matsumoto <jpmat296@gmail.com>
2025-02-26 17:55:59 +0100

    Support metrics in index template dump

--- scripts/dump_index_templates.py
+++ scripts/dump_index_templates.py
@@ -4,25 +4,27 @@ import json
 
 es = Elasticsearch('https://elastic:change_me@localhost:9200/', verify_certs=False)
 
-cts = es.cluster.get_component_template(name='logs-haproxy.log@package')
-print(f"{len(cts['component_templates'])} component templates found.")
+cts = list()
+cts.extend(es.cluster.get_component_template(name='logs-haproxy.*@package')['component_templates'])
+cts.extend(es.cluster.get_component_template(name='metrics-haproxy.*@package')['component_templates'])
+print(f"{len(cts)} component templates found.")
 
-tmpls = es.indices.get_index_template(name='logs-haproxy.log')
-print(f"{len(tmpls['index_templates'])} index templates found.")
-
-# FIXME: add metrics
+tmpls = list()
+tmpls.extend(es.indices.get_index_template(name='logs-haproxy.*')['index_templates'])
+tmpls.extend(es.indices.get_index_template(name='metrics-haproxy.*')['index_templates'])
+print(f"{len(tmpls)} index templates found.")
 
 with open("dev_tools_index.txt", "w") as outfile: 
-    outfile.write("# After below commands, 1 component template  + 1 index template exist\n")
+    outfile.write(f"# After below commands, {len(cts)} component template(s) + {len(tmpls)} index template(s) exist(s)\n")
     outfile.write("# To check:\n")
     outfile.write("GET _component_template/*-haproxy.*?filter_path=*.name\n")
     outfile.write("GET _index_template/*-haproxy.*?filter_path=*.name\n")
-    for ct in cts['component_templates']:
+    for ct in cts:
         outfile.write("\n")
         outfile.write(f"PUT _component_template/{ct['name']}\n")
         json.dump(ct['component_template'], outfile, indent=2)
         outfile.write("\n")
-    for tmpl in tmpls['index_templates']:
+    for tmpl in tmpls:
         outfile.write("\n")
         outfile.write(f"PUT _index_template/{tmpl['name']}\n")
         json.dump(tmpl['index_template'], outfile, indent=2)
