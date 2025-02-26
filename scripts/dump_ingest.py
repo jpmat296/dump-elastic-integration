b090eae991eaf3dc41b23821688712444b937c6e
Jean-Pierre Matsumoto <jpmat296@gmail.com>
2025-02-26 23:31:15 +0100

    Support metrics in ingest pipelines dump

--- scripts/dump_ingest.py
+++ scripts/dump_ingest.py
@@ -4,15 +4,20 @@ import json
 
 es = Elasticsearch('https://elastic:change_me@localhost:9200/', verify_certs=False)
 
-igs = es.ingest.get_pipeline(id='logs-haproxy.*')
-print(f"{len(igs.body.keys())} ingest pipelines found.")
+igs = list()
+res_ingest = es.ingest.get_pipeline(id='metrics-haproxy.*')
+igs.extend([{key: val} for key, val in res_ingest.body.items()])
+res_ingest = es.ingest.get_pipeline(id='logs-haproxy.*')
+igs.extend([{key: val} for key, val in res_ingest.body.items()])
+print(f"{len(igs)} ingest pipelines found.")
 
 with open("dev_tools_ingest.txt", "w") as outfile: 
-    outfile.write("# After below commands, 4 pipelines exist")
+    outfile.write(f"# After below commands, {len(igs)} pipeline(s) exist(s)\n")
     outfile.write("# To check:\n")
-    outfile.write("GET _ingest/pipeline/*-infoblox_nios.*?filter_path=*.description\n")
-    for ig in igs.body.keys():
+    outfile.write("GET _ingest/pipeline/*-haproxy.*?filter_path=*.description\n")
+    for ig in igs:
+        iname = next(iter(ig.keys()))
         outfile.write("\n")
-        outfile.write(f"PUT _ingest/pipeline/{ig}\n")
-        json.dump(igs[ig], outfile, indent=2)
+        outfile.write(f"PUT _ingest/pipeline/{iname}\n")
+        json.dump(ig[iname], outfile, indent=2)
         outfile.write("\n")
